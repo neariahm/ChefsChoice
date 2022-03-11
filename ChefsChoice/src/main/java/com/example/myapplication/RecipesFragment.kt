04ba8ba11e218f3824b79d3a41b8d5558c18e68c
Meme Scott <82485727+employeeofchoice@users.noreply.github.com>
2022-03-11 14:01:50 -0500

    Updated MLKit file

--- ChefsChoice/src/main/java/com/example/myapplication/RecipesFragment.kt
+++ ChefsChoice/src/main/java/com/example/myapplication/RecipesFragment.kt
@@ -1,18 +1,36 @@
 package com.example.myapplication
 
+import android.app.Activity
+import android.content.ActivityNotFoundException
+import android.content.Context.CAMERA_SERVICE
+import android.content.Intent
+import android.graphics.Bitmap
+import android.hardware.camera2.CameraCharacteristics
+import android.hardware.camera2.CameraManager
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
 import android.widget.SearchView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil.setContentView
 import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.myapplication.databinding.FragmentRecipesBinding
 import com.example.myapplication.databinding.RecipeListBinding
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.mlkit.vision.barcode.BarcodeScannerOptions
+import com.google.mlkit.vision.barcode.BarcodeScanning
 import com.google.mlkit.vision.barcode.common.Barcode
 import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.label.ImageLabeling
+import com.google.mlkit.vision.label.defaults.ImageLabelerOptions
 
 class RecipesFragment : Fragment() {
 
@@ -29,7 +47,7 @@ class RecipesFragment : Fragment() {
 
         binding.searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
             override fun onQueryTextSubmit(query: String): Boolean {
-            Log.i("Neariah", query)
+                Log.i("Neariah", query)
                 viewModel.getChefPhotos(query)
                 return true
             }
@@ -40,6 +58,7 @@ class RecipesFragment : Fragment() {
         })
         return binding.root
     }
+}
 /*  Would code related to the camera go here instead? https://developers.google.com/ml-kit/vision/barcode-scanning/android
     val options = BarcodeScannerOptions.Builder()
         .setBarcodeFormats(
@@ -48,11 +67,14 @@ class RecipesFragment : Fragment() {
         .build()
 
          */
+//Tameika Testing MLK changes
+
+// Inflate the layout for this fragment
+        //return inflater.inflate(R.layout.fragment_recipes, container, false)}
 
 
 
-        // Inflate the layout for this fragment
-        //return inflater.inflate(R.layout.fragment_recipes, container, false)
-    }
 
-//}
+
+
+

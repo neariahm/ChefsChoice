23757129a8f8f205cfe644a279d456b86ee848ce
williamsonb <43324783+williamsonb@users.noreply.github.com>
2022-03-12 19:04:57 -0600

    database2 branch

--- ChefsChoice/src/main/java/com/example/myapplication/RecipesFragment.kt
+++ ChefsChoice/src/main/java/com/example/myapplication/RecipesFragment.kt
@@ -7,16 +7,25 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.SearchView
+import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.GridLayoutManager
+import com.example.myapplication.database.RecipesApplication
 import com.example.myapplication.databinding.FragmentRecipesBinding
 import com.example.myapplication.databinding.RecipeListBinding
+import com.example.myapplication.viewmodel.RecipesViewModelFactory
+import com.google.android.material.snackbar.Snackbar
 import com.google.mlkit.vision.barcode.BarcodeScannerOptions
 import com.google.mlkit.vision.barcode.common.Barcode
 import com.google.mlkit.vision.common.InputImage
+import kotlinx.coroutines.launch
 
 class RecipesFragment : Fragment() {
-
-    private val viewModel: RecipesViewModel by viewModels()
+    val adapter = RecipesListAdapter()
+    private val viewModel: RecipesViewModel by activityViewModels() {
+        RecipesViewModelFactory((activity?.application as RecipesApplication).database.favoriteDao())
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,11 +34,10 @@ class RecipesFragment : Fragment() {
         val binding = FragmentRecipesBinding.inflate(inflater)
         binding.lifecycleOwner = this
         binding.viewModel = viewModel
-        binding.recyclerView.adapter = RecipesListAdapter()
 
         binding.searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
             override fun onQueryTextSubmit(query: String): Boolean {
-            Log.i("Neariah", query)
+                Log.i("Neariah", query)
                 viewModel.getChefPhotos(query)
                 return true
             }
@@ -38,8 +46,22 @@ class RecipesFragment : Fragment() {
                 return false
             }
         })
+        binding.recyclerView.adapter = adapter
+        adapter.onSaveClickListener {
+            lifecycleScope.launch {
+                viewModel.insert(it)
+                Snackbar.make(requireView(), "SAVED", Snackbar.LENGTH_SHORT).show()
+            }
+        }
         return binding.root
     }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
 /*  Would code related to the camera go here instead? https://developers.google.com/ml-kit/vision/barcode-scanning/android
     val options = BarcodeScannerOptions.Builder()
         .setBarcodeFormats(

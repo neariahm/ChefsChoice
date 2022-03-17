3df5d69182c773684cf031834dfda3eda9899d83
Tyrees Carney <Tyrees34@gmail.com>
2022-03-17 15:18:23 -0700

    commit and push login

--- ChefsChoice/src/main/java/com/example/myapplication/MainActivity.kt
+++ ChefsChoice/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,20 +3,83 @@ package com.example.myapplication
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
+    private lateinit var emailEt: EditText
+    private lateinit var passwordEt: EditText
+
+    private lateinit var signUpBtn: Button
+    private lateinit var loginBtn: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val secondActbutton = findViewById<Button>(R.id.secondActivityBtn)
-        secondActbutton.setOnClickListener {
+        auth = FirebaseAuth.getInstance()
+
+        emailEt = findViewById(R.id.email_edt_text)
+        passwordEt = findViewById(R.id.pass_edt_text)
+
+        loginBtn = findViewById(R.id.login_btn)
+        signUpBtn = findViewById(R.id.signup_btn)
+
+        signUpBtn.setOnClickListener{
+            var email: String = emailEt.text.toString()
+            var password: String = passwordEt.text.toString()
+
+            if(TextUtils.isEmpty(email) || TextUtils.isEmpty(password)) {
+                Toast.makeText(this, "Please fill all the fields", Toast.LENGTH_LONG).show()
+            } else{
+                auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this, OnCompleteListener{ task ->
+                    if(task.isSuccessful){
+                        Toast.makeText(this, "Successfully Registered", Toast.LENGTH_LONG).show()
+                        val intent = Intent(this, MainActivity2::class.java)
+                        startActivity(intent)
+                        finish()
+                    }else {
+                        Toast.makeText(this, "Registration Failed", Toast.LENGTH_LONG).show()
+                    }
+                })
+            }
+        }
+
+        loginBtn.setOnClickListener{
             val intent = Intent(this, MainActivity2::class.java)
             startActivity(intent)
+            finish()
+        }
+
+        loginBtn.setOnClickListener {
+            var email: String = emailEt.text.toString()
+            var password: String = passwordEt.text.toString()
+
+            if(TextUtils.isEmpty(email) || TextUtils.isEmpty(password)) {
+                Toast.makeText(this@MainActivity, "Please fill all the fields", Toast.LENGTH_LONG).show()
+            } else{
+                auth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, OnCompleteListener { task ->
+                    if(task.isSuccessful) {
+                        Toast.makeText(this, "Successfully Logged In", Toast.LENGTH_LONG).show()
+                        val intent = Intent(this, MainActivity2::class.java)
+                        startActivity(intent)
+                        finish()
+                    }else {
+                        Toast.makeText(this, "Login Failed", Toast.LENGTH_LONG).show()
+                    }
+                })
+            }
+        }
 
         }
+
     }
 
-}
 
